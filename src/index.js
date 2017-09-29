ba6feb13c3edc124633021faa8aeba9a64e16479
Ekaterina Raylyan <31958491+kotiknalune@users.noreply.github.com>
2017-09-29 16:23:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function multiply(first, second) {
-    return parseInt(first) * parseInt(second) + ' ';
+    first = a.split('').reverse();
+	second = b.split('').reverse();
+	var arr = [];
+
+	for (var i = 0; i < a.length; i++) {
+		for (var j = 0; j < b.length; j++) {
+			if (!arr[i+j]) {
+				arr[i+j] = 0;
+			}
+			arr[i+j] += a[i] * b[j]
+		}
+	}
+	
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] > 9) {
+			if (!arr[i+1]) {
+				arr[i+1] = 0;
+			}
+			arr[i+1] += parseInt(arr[i] / 10);
+			arr[i] = arr[i] % 10;
+		}
+	}
+
+	var str = arr.reverse().join('');
+	return str;
 }
